63099eb19d25ffc5b5ccf266c9e28a643a2ee28c
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2021-05-21 10:26:58 +0900

    リーティングの作成とパス作成

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -7,16 +7,14 @@
     <table class="table table-striped">
       <tr>
         <th>記事番号</th>
-        <th>日付</th>
         <th>タイトル</th>
-        <th>内容</th>
+        <th>日付</th>
       </tr>
       @foreach($blogs as $blog)
       <tr>
         <td>{{ $blog->id }}</td>
+        <td><a href="/blog/{{ $blog->id }}">{{ $blog->title }}</a></td>
         <td>{{ $blog->updated_at }}</td>
-        <td>{{ $blog->title }}</td>
-        <td>{{ $blog->content }}</td>
       </tr>
       @endforeach 
     </table>
